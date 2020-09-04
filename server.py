9415aaefa87e776c430c475bac9e954d555c3004
Benjamin Choy <bench2004@gmail.com>
2020-09-04 19:54:13 +0800

    Update server.py

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ from pprint import pprint
 
 DARK_SKY = {
     "api_url": "https://api.darksky.net/forecast",
-    "img_url": "https://darksky.net/images/weather-icons",
+    "img_url": "http://mqtt.local/icons",
     "img_ext": "png"
 }
 
